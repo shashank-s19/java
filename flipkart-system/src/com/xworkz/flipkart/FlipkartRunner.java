7bb415595ee5a618c417e7d31d9a506dec37c109
shashank-s19 <shashank.xworkz@outlook.com>
2025-04-09 11:59:28 +0530

    Update FlipkartRunner.java

--- flipkart-system/src/com/xworkz/flipkart/FlipkartRunner.java
+++ flipkart-system/src/com/xworkz/flipkart/FlipkartRunner.java
@@ -8,10 +8,10 @@ public class FlipkartRunner {
 
     public static void main(String[] args) {
         FlipkartDto flipkartDto = new FlipkartDto();
-        flipkartDto.setFullName("charan hJ");
+        flipkartDto.setFullName("shashank");
         flipkartDto.setMobileNumber("96392285282");
-        flipkartDto.setEmail("charan@gmil.com");
-        flipkartDto.setPassword("charan@2025");
+        flipkartDto.setEmail("shashank@gmil.com");
+        flipkartDto.setPassword("shashank@2025");
         flipkartDto.setShippingAddress("Bangalore, India");
 
         FlipkartApp flipkartApp = new FlipkartApp();
